b9583e4e26d8cc702337215f333194d1993b9c57
tian diesel <guardor@163.com>
2018-01-09 11:07:33 +0800

    Say something

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -38,7 +38,7 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Hello Laravel! - by vin diesel</div>
+                <div class="title">Hello Laravel! - by tian diesel </div>
             </div>
         </div>
     </body>
